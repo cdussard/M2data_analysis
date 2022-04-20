927558c6db410691118f32e437701dd7b75d845f
cdussard <37064773+cdussard@users.noreply.github.com>
2022-04-20 19:40:16 +0200

    implement permutation t test

---
+++ analyse_M2/permutation_F_test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 20 19:22:01 2022
+
+@author: claire.dussard
+"""
+import pandas as pd
+import mne
+douzeQuinzeHzData = pd.read_csv("./data/Jasp_anova/ANOVA_12_15Hz_C3_long.csv")
+df_douzeQuinzeHzData=douzeQuinzeHzData.iloc[: , 1:]
+df_douzeQuinzeHzData_mP = df_douzeQuinzeHzData.iloc[:,0:2]
+mne.stats.permutation_t_test(df_douzeQuinzeHzData_mP, n_permutations=10000, tail=0)
