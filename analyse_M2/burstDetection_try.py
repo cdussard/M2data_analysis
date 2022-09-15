3b5f16648770afcba01bc390911a42e29d8e5fff
cdussard <cdussard@ensc.fr>
2022-09-15 13:02:10 +0200

    Update burstDetection_try.py

--- analyse_M2/burstDetection_try.py
+++ analyse_M2/burstDetection_try.py
@@ -27,18 +27,20 @@ elec = "C3"
 #f_range = (8, 12)# f_range = (15,30)
 epochs = EpochDataMain
 
-def compute_stats_bursts(f_range,elec,epochs,plot,n_sujet):
+def compute_stats_bursts(f_range,elec,epochs,plot,n_sujet,tmin,tmax):
+    epochs = epochs[n_sujet].crop(tmin=tmin,tmax=tmax)
     n_bursts = []
     dur_burst = []
-    for i in range(len(epochs[n_sujet])):
-        pos_C3 = epochs[n_sujet].info.ch_names.index(elec)
-        if epochs[n_sujet].info.ch_names[pos_C3]==elec:
-            ep = epochs[n_sujet][i]._data[:,pos_C3]
+    for i in range(len(epochs)):
+        pos_C3 = epochs.info.ch_names.index(elec)
+        if epochs.info.ch_names[pos_C3]==elec:
+            ep = epochs[i]._data[:,pos_C3]
             if ep.shape[0]==1:
                 sig = np.mean(ep,axis=0)
             else:
                 sig = ep 
             # Set sampling rate, and create a times vector for plotting
+            print(sig.shape)
             fs = 1000
             times = create_times(len(sig)/fs, fs)
             min_n_cycles = 3
@@ -54,15 +56,21 @@ def compute_stats_bursts(f_range,elec,epochs,plot,n_sujet):
             print("wrong channel")
     return n_bursts,dur_burst
 
-f_range = (8, 12)
-for i in range(20,22):
-    n_bursts_m,dur_burst_m = compute_stats_bursts(f_range,elec,EpochDataMain,False,i)
-    n_bursts_p,dur_burst_p = compute_stats_bursts(f_range,elec,EpochDataPendule,False,i)
-    plt.plot(range(len(n_bursts_m)),n_bursts_m,label="n_bursts_main"+str(i))
-    plt.plot(range(len(n_bursts_p)),n_bursts_p,label="n_bursts_pendule"+str(i))
-    plt.legend()
-    print(np.mean(n_bursts_m))
-    print(np.mean(n_bursts_p))
-    print(np.mean(dur_burst_m))
-    print(np.mean(dur_burst_p))
-raw_signal.plot(block=True)
+f_range = (11, 15)
+ls_styles = ["--",":","solid"]
+for j in range(11,12):
+    s = 0
+    for i in range(j,j+2):
+        n_bursts_m,dur_burst_m = compute_stats_bursts(f_range,elec,EpochDataMain,False,i,1.5,25)
+        n_bursts_p,dur_burst_p = compute_stats_bursts(f_range,elec,EpochDataPendule,False,i,1.5,25)
+        plt.plot(range(len(n_bursts_m)),n_bursts_m,label="n_bursts_main"+str(i),color="g",ls=ls_styles[s])
+        plt.plot(range(len(n_bursts_p)),n_bursts_p,label="n_bursts_pendule"+str(i),color="r",ls=ls_styles[s])
+        plt.axhline(np.mean(n_bursts_m),color="g",ls=ls_styles[s])
+        plt.axhline(np.mean(n_bursts_p),color="r",ls=ls_styles[s])
+        plt.legend()
+        print(np.mean(n_bursts_m))
+        print(np.mean(n_bursts_p))
+        print(np.mean(dur_burst_m))
+        print(np.mean(dur_burst_p))
+        s += 1
+    raw_signal.plot(block=True)
